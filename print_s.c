edac9b861be4425167340330da74a941b8a34b8f
vts3 <frankmoralescruz@gmail.com>
2022-10-14 02:32:28 +0000

    function to print a string

---
+++ print_s.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_s - print a string
+ * @s: string to print
+ *
+ * Return: i
+ */
+
+int print_s(va_list s)
+{
+	char *str = va_arg(s, char *);
+	int i;
+
+	if (!str)
+		str = "(null)";
+	for (i = 0; str[i]; i++)
+		_putchar(str[i]);
+	return(i);
+}
