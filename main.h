7f40af9c310819b9951b29fbdb6eb5dadd5f5c5f
vts3 <frankmoralescruz@gmail.com>
2022-10-14 02:19:00 +0000

    fixing header file

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct print - print type and print function
+ * @c: print type
+ * @f: print function
+ */
+typedef struct print
+{
+	char *c;
+	int (*f)(va_list);
+} print_f;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int (*get_option(const char *format))(va_list);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_d(va_list d);
+int print_i(va_list i);
+int print_binary(va_list b);
+
+#endif
