a17b0189a330179e2824a88a5365ee7a6de77ada
vts3 <frankmoralescruz@gmail.com>
2022-10-14 02:25:37 +0000

    fixing header file

--- main.h
+++ main.h
@@ -2,15 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
-<<<<<<< HEAD
 #include <stdlib.h>
 #include <stdarg.h>
 
-/**
- * struct print - print type and print function
- * @c: print type
- * @f: print function
- */
 typedef struct print
 {
 	char *c;
@@ -25,18 +19,5 @@ int print_s(va_list s);
 int print_d(va_list d);
 int print_i(va_list i);
 int print_binary(va_list b);
-=======
-#include <sring.h>
-#include <stdlib.h>
-
-int _printf(const char *format, ...);
-
-typedef struct data
-{
-  char *type;
-  int (*f)(va_list ptr);
-}
-data;
->>>>>>> ae6d0755554d921cf798896ef26acd61c5c400ea
 
 #endif
